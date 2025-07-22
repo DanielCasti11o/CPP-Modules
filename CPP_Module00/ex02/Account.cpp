d30be6407e9d2f2120a109da659332f0e734c4d9
Daniel Castillo Correa <dacastil@c1r11s1.42madrid.com>
2025-07-22 16:15:50 +0200

    Constructor & Destructor

--- CPP_Module00/ex02/Account.cpp
+++ CPP_Module00/ex02/Account.cpp
@@ -3,21 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel-castillo <daniel-castillo@studen    +#+  +:+       +#+        */
+/*   By: dacastil <dacastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 23:28:56 by daniel-cast       #+#    #+#             */
-/*   Updated: 2025/07/18 23:30:40 by daniel-cast      ###   ########.fr       */
+/*   Updated: 2025/07/19 14:43:16 by dacastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
 
 Account::Account(int initial_deposit)
 {
-
+	_accountIndex = _nbAccounts++;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_totalAmount += initial_deposit;
+	std::cout << "time" << std::endl << "index:" << _accountIndex << ";amount:" << _amount << ";created" <<std::endl;
 }
 
 Account::~Account(void)
 {
-
+	std::cout << "time" << std::endl << "index:" << _accountIndex << ";amount:" << _amount << ";closed" <<std::endl;
 }
+
+// Getters
+
+// void	Account::getNbAccounts()
+// {
+
+// }
