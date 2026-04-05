aa8dc20f4e5d83e243705adc1d8ceae6ba9960b7
DanielCasti11o <bydanny17@gmail.com>
2026-04-05 18:00:40 +0200

    Init cpp 07

---
+++ CPP_Module07/ex00/whatever.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-castillo <daniel-castillo@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/05 17:58:12 by daniel-cast       #+#    #+#             */
+/*   Updated: 2026/04/05 17:59:01 by daniel-cast      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+# include <string>
+
+class whatever
+{
+private:
+	/* data */
+public:
+	whatever(/* args */);
+	~whatever();
+};
+
+
+#endif
