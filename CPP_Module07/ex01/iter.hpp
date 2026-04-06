e8122d499a3a5187b40c24e73669ba9ae78c446a
Daniel Castillo Correa <dacastil@c3r2s3.42madrid.com>
2026-04-06 19:13:35 +0200

    cpp07ex01, in progress...

--- CPP_Module07/ex01/iter.hpp
+++ CPP_Module07/ex01/iter.hpp
@@ -3,15 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel-castillo <daniel-castillo@studen    +#+  +:+       +#+        */
+/*   By: dacastil <dacastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 18:40:23 by daniel-cast       #+#    #+#             */
-/*   Updated: 2026/04/05 18:40:44 by daniel-cast      ###   ########.fr       */
+/*   Updated: 2026/04/06 18:18:45 by dacastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
+
+# include <iostream>
+# include <cstdlib>
+
+// Version NO_CONST
+
+template<typename T>
+void	iter(T* array, size_t length, void (*func)(T&));
+
+// Version CONST
 
 
 
